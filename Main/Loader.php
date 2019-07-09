fd3a9541680ad32db304cfae6474688cea6ac509
andikaryanto11 <andik.aryanto11@gmail.com>
2019-07-09 16:46:08 +0700

    pdf
    pdf

--- Main/Loader.php
+++ Main/Loader.php
@@ -11,7 +11,8 @@ $load = [
  */
 
 include APP_PATH."3rdParty/phpqrcode/qrlib.php";
-include APP_PATH."3rdParty/fpdf/fpdf.php";
+// include APP_PATH."3rdParty/fpdf/fpdf.php";
+include APP_PATH."3rdParty/TCPDF/tcpdf.php";
 foreach($load as $key => $value){
     foreach ($value as $v){
         // echo APP_PATH.$key."\\".$v.".php";
--- Pages/laporan/cetakformulir.php
+++ Pages/laporan/cetakformulir.php
@@ -1,8 +1,71 @@
+
 <?php
-    $pdf = new FPDF();
-    $pdf->AddPage();
-    $pdf->SetFont('Arial', 'B', '14');
-    $pdf->Cell(0,20, 'PENERIMAAN SISWA BARU SMP MUH 1 MLATI', '0', 1, 'L');
-    $pdf->Cell(0,10, 'jalan Magelang km 7.5, Sendangadi Mlati ', '0', 1, 'L');
-    $pdf->Output();
-?>
+//============================================================+
+// File name   : example_048.php
+// Begin       : 2009-03-20
+// Last Update : 2013-05-14
+//
+// Description : Example 048 for TCPDF class
+//               HTML tables and table headers
+//
+// Author: Nicola Asuni
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com LTD
+//               www.tecnick.com
+//               info@tecnick.com
+//============================================================+
+
+/**
+ * Creates an example PDF TEST document using TCPDF
+ * @package com.tecnick.tcpdf
+ * @abstract TCPDF - Example: HTML tables and table headers
+ * @author Nicola Asuni
+ * @since 2009-03-20
+ */
+
+// Include the main TCPDF library (search for installation path).
+// require_once('tcpdf_include.php');
+
+// create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('Nicola Asuni');
+$pdf->SetTitle('TCPDF Example 048');
+$pdf->SetSubject('TCPDF Tutorial');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+// // set default header data
+// $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 048', PDF_HEADER_STRING);
+
+// // set header and footer fonts
+// $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+// $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+// // set default monospaced font
+// $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// // set margins
+// $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+// $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+// $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+// // set auto page breaks
+// $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// // set image scale factor
+// $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+
+
+// // -----------------------------------------------------------------------------
+
+// //Close and output PDF document
+$pdf->Output('example_048.pdf', 'I');
+
+//============================================================+
+// END OF FILE
+//============================================================+
