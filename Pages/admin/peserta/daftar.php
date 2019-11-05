2b6415146afae95b8c814b4123c7174ddfd39d85
andikaryanto11 <andik.aryanto11@gmail.com>
2019-11-05 08:58:35 +0700

    many
    many

--- Pages/admin/peserta/daftar.php
+++ Pages/admin/peserta/daftar.php
@@ -1,153 +1,167 @@
-<?php include APP_PATH.'Pages/headeradmin.php';
-    $pengaturan = ambilpengaturan();
-        
+<?php include APP_PATH . 'Pages/headeradmin.php';
+$pengaturan = ambilpengaturan();
+
 ?>
 
-    <!-- Left side column. contains the logo and sidebar -->
+<!-- Left side column. contains the logo and sidebar -->
 
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <h1>
-                BERANDA
-                <small>CONTROL PANEL</small>
-            </h1>
-        </section>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            BERANDA
+            <small>CONTROL PANEL</small>
+        </h1>
+    </section>
 
-        <!-- Main content -->
+    <!-- Main content -->
     <section class="content">
-      <div class="row">
-        <div class="col-xs-12">
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Daftar Peserta</h3>
-              
-                </div>
-            <!-- /.box-header -->
-                <div class="box-body">
-                
-                    <table id="tablepengguna" class="table table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th>No Daftar</th>
-                                <th>NISN</th>
-                                <th>Nama Lengkap</th>
-                                <th>JenKel</th>
-                                <th>No Telp</th>
-                                <th>Asal Sekolah</th>
-                                <?php if(isset($_GET['halaman']) && $_GET['halaman'] == "siswaditerima"){
-                                    echo "<th>Daftar Ulang?</th>";
-                                }?>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody> 
-                            <?php
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Daftar Peserta</h3>
+                        <?php if (isset($_GET['halaman']) && $_GET['halaman'] == "siswaditerima") { ?>
+                            <div class="row">
+                                <div class="col-xs-12 text-right">
+                                    <a href = "<?= $url . "Pages/laporan/pesertaditerimaexc.php" ?>" class="btn btn-primary"><i class = "fa fa-file-excel-o"></i> Eksport</a>
+                                </div>
+                            </div>
+                        <?php } ?>
+
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+
+                        <table id="tablepengguna" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No Daftar</th>
+                                    <th>NISN</th>
+                                    <th>Nama Lengkap</th>
+                                    <th>JenKel</th>
+                                    <th>No Telp</th>
+                                    <th>Asal Sekolah</th>
+                                    <?php if (isset($_GET['halaman']) && $_GET['halaman'] == "siswaditerima") {
+                                        echo "<th>Daftar Ulang?</th>";
+                                    } ?>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
                                 $peserta = array();
 
-                                if(isset($_GET['halaman'])){
+                                if (isset($_GET['halaman'])) {
 
-                                
-                                    switch($_GET['halaman']){
 
-                                        case "siswaditerima" : 
-                                            if(date_create(tanggalSekarang()) >= date_create($pengaturan['TglPengumuman']))
+                                    switch ($_GET['halaman']) {
+
+                                        case "siswaditerima":
+                                            if (date_create(tanggalSekarang()) >= date_create($pengaturan['TglPengumuman']))
                                                 $peserta = pesertaditerima();
                                             break;
-                                        case "siswaditolak" :
-                                            if(date_create(tanggalSekarang()) >= date_create($pengaturan['TglPengumuman']))
+                                        case "siswaditolak":
+                                            if (date_create(tanggalSekarang()) >= date_create($pengaturan['TglPengumuman']))
                                                 $peserta = pesertaditolak();
                                             break;
-                                        default : 
+                                        default:
                                             $peserta = ambilpeserta();
                                             break;
-
                                     }
                                 }
 
-                                foreach($peserta as $p){
-                            ?>
-                            <tr>
-                                <td><?= $p['NoDaftar']?></a></td>
-                                <td><?= $p['NISN']?></a></td>
-                                <td><?= $p['NamaLengkap'] ?></td>
-                                <td><?= $p['JenisKelamin'] ?></td>
-                                <td><?= $p['NoTelp'] ?></td>
-                                <td><?= $p['AsalSekolah'] ?></td>
-                                <?php if(isset($_GET['halaman']) && $_GET['halaman'] == "siswaditerima"){
-                                    // echo "<th>{$p['Daftarulang']}</th>";
-                                    if( $p['Daftarulang'] == 1)
-                                        echo "<td><input id='{$p['Id']}' type='checkbox' class='minimal' checked></td>";
-                                    else 
-                                        echo "<td><input id='{$p['Id']}' type='checkbox' class='minimal'></td>";
-                                }?>
-                                <td><a href="<?= $url."Pages/admin/peserta/detail.php?NoRegist={$p['NoDaftar']}"?>"><i class = "fa fa-info"></i></a></td>
-                            </tr>
-                            <?php
-                                }
-                            ?>
+                                foreach ($peserta as $p) {
+                                    ?>
+                                        <tr>
+                                            <td><?= $p['NoDaftar'] ?></a></td>
+                                            <td><?= $p['NISN'] ?></a></td>
+                                            <td><?= $p['NamaLengkap'] ?></td>
+                                            <td><?= $p['JenisKelamin'] ?></td>
+                                            <td><?= $p['NoTelp'] ?></td>
+                                            <td><?= $p['AsalSekolah'] ?></td>
+                                            <?php if (isset($_GET['halaman']) && $_GET['halaman'] == "siswaditerima") {
+                                                    // echo "<th>{$p['Daftarulang']}</th>";
+                                                    if ($p['Daftarulang'] == 1)
+                                                        echo "<td><input id='{$p['Id']}' type='checkbox' class='minimal' checked></td>";
+                                                    else
+                                                        echo "<td><input id='{$p['Id']}' type='checkbox' class='minimal'></td>";
+                                                } ?>
+                                            <td><a href="<?= $url . "Pages/admin/peserta/detail.php?NoRegist={$p['NoDaftar']}" ?>"><i class="fa fa-info"></i></a></td>
+                                        </tr>
+                                    <?php
+                                    }
+                                    ?>
 
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                            <th>No Daftar</th>
-                                <th>NISN</th>
-                                <th>Nama Lengkap</th>
-                                <th>JenKel</th>
-                                <th>No Telp</th>
-                                <th>Asal Sekolah</th>
-                                <?php if(isset($_GET['halaman']) && $_GET['halaman'] == "siswaditerima"){
-                                    echo "<th>Daftar Ulang?</th>";
-                                }?>
-                                <th class = "disabled-sorting text-right">Aksi</th>
-                            </tr>
-                        </tfoot>
-                    </table>
-            <!-- /.box-body -->
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th>No Daftar</th>
+                                    <th>NISN</th>
+                                    <th>Nama Lengkap</th>
+                                    <th>JenKel</th>
+                                    <th>No Telp</th>
+                                    <th>Asal Sekolah</th>
+                                    <?php if (isset($_GET['halaman']) && $_GET['halaman'] == "siswaditerima") {
+                                        echo "<th>Daftar Ulang?</th>";
+                                    } ?>
+                                    <th class="disabled-sorting text-right">Aksi</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                        <!-- /.box-body -->
+                    </div>
                 </div>
+                <!-- /.box -->
             </div>
-          <!-- /.box -->
-        </div>
-      <!-- /.row -->
+            <!-- /.row -->
     </section>
 </div>
-    
-<script type = "text/javascript">
+
+<script type="text/javascript">
     var table;
     // function datatable(){
     $('#tablepengguna').DataTable({
-        "columnDefs": [ 
-            {
-            targets: 'disabled-sorting', 
+        "columnDefs": [{
+            targets: 'disabled-sorting',
             orderable: false
-            }
-        ],
-    }); 
+        }],
+    });
 
     $(':checkbox').change(function() {
-    // this will contain a reference to the checkbox   
-    if (this.checked) {
-        console.log(this.checked);
-    
-    } else {
-        console.log(this.checked);
-    }
+        // this will contain a reference to the checkbox   
+        if (this.checked) {
+            console.log(this.checked);
 
-    $.ajax({
-        url: '<?= $url."Fungsi/Aksi/Daftarulang.php" ?>',
-        type : "POST",
-        data : {
-            Id : this.id,
-            Selected : this.checked
-        },
-        success : function(result){
-            var res = JSON.parse(result)
-            console.log(res);
+        } else {
+            console.log(this.checked);
         }
-    })
-});
+
+        $.ajax({
+            url: '<?= $url . "Fungsi/Aksi/Daftarulang.php" ?>',
+            type: "POST",
+            data: {
+                Id: this.id,
+                Selected: this.checked
+            },
+            success: function(result) {
+                var res = JSON.parse(result)
+                console.log(res);
+            }
+        })
+    });
+
+    // $("#export").on("click", function(){
+    //     $.ajax({
+    //         url: '<?= $url . "Pages/laporan/pesertaditerimaexc.php" ?>',
+    //         type: "POST",
+    //         success : function(result){
+    //             console.log(result);
+    //         }
+    //     });
+    // })
 
     // }
 </script>
-<?php include APP_PATH.'Pages/footeradmin.php';?>
+<?php include APP_PATH . 'Pages/footeradmin.php'; ?>
