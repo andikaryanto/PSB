e733bd40bc93fd4c9f2034a82a102a638c0ca7c6
andikaryanto11 <andik.aryanto11@gmail.com>
2019-11-20 16:37:48 +0700

    Update daftarulangpeserta.php

--- Pages/daftarulangpeserta.php
+++ Pages/daftarulangpeserta.php
@@ -5,57 +5,63 @@
             <div class="box">
                 <?php
 
-                $peserta = ambilhanyapeserta("WHERE NoDaftar = '{$_POST['nomorpendaftaran']}'");
-                if ($peserta) {
-                    $daftarulang = ambildafhanyatarulang("AND a.Id = {$peserta['Id']}");
-                    $content = $url . "Pages/daftartab.php?key=" . encrypt("edit." . $kuncirahasia . "." . $peserta['NoDaftar']);
-                    $text = generateQR($url, $content, $peserta['NoDaftar'] . $qrcode['registrasi']);
-                    if ($daftarulang && $daftarulang['DaftarUlang'] == 1) {
-                        ?>
+                $pengaturan = ambilpengaturan();
+                if (date_create(tanggalSekarang()) >= date_create($pengaturan['TglPengumuman'])) {
 
-                        <h3 align="center">ANDA SUDAH DAFTAR ULANG </h3>
-
-                    <?php
-
-                        } else if ($daftarulang && $daftarulang['DaftarUlang'] == 0) {
-
+                    $peserta = ambilhanyapeserta("WHERE NoDaftar = '{$_POST['nomorpendaftaran']}'");
+                    if ($peserta) {
+                        $daftarulang = ambildafhanyatarulang("AND a.Id = {$peserta['Id']}");
+                        $content = $url . "Pages/daftartab.php?key=" . encrypt("edit." . $kuncirahasia . "." . $peserta['NoDaftar']);
+                        $text = generateQR($url, $content, $peserta['NoDaftar'] . $qrcode['registrasi']);
+                        if ($daftarulang && $daftarulang['DaftarUlang'] == 1) {
                             ?>
-                        <h3 align="center">ANDA BELUM DAFTAR ULANG </h3>
-                    <?php
-                        } else {
-                            ?>
+
+                            <h3 align="center">ANDA SUDAH DAFTAR ULANG </h3>
+
+                        <?php
+
+                                } else if ($daftarulang && $daftarulang['DaftarUlang'] == 0) {
+
+                                    ?>
+                            <h3 align="center">ANDA BELUM DAFTAR ULANG </h3>
+                        <?php
+                                } else {
+                                    ?>
                             <h3 align="center">ANDA TIDAK DITERIMA </h3>
-                            <?php
-                        }
-                        ?>
-                    <div class="callout">
+                        <?php
+                                }
+                                ?>
+                        <div class="callout">
 
-                        <div class="row">
-                            <div class="col-md-2">
-                                <img src="<?= $text ?>" style="height:120; width:120 !important">
-                            </div>
-                            <div class="col-md-2">
-                                <img src="<?= "../" . $peserta['UrlPhoto'] ?>" style="height:120; width:120 !important">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="box-body">
-                                    <p> No Registrasi : <b><?= $peserta['NoDaftar'] ?></b></p>
-                                    <p> Nama Peserta : <b><?= $peserta['NamaLengkap'] ?></b></p>
-                                    <p> TTL : <b><?= $peserta['TempatLahir'] . ", " . $peserta['TglLahir'] ?></b></p>
-                                    <p> Jenis Kelamin : <b><?= $peserta['JenisKelamin'] ?></b></p>
-                                    <p> Sekolah Asal : <b><?= $peserta['AsalSekolah'] ?></b></p>
+                            <div class="row">
+                                <div class="col-md-2">
+                                    <img src="<?= $text ?>" style="height:120; width:120 !important">
+                                </div>
+                                <div class="col-md-2">
+                                    <img src="<?= "../" . $peserta['UrlPhoto'] ?>" style="height:120; width:120 !important">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="box-body">
+                                        <p> No Registrasi : <b><?= $peserta['NoDaftar'] ?></b></p>
+                                        <p> Nama Peserta : <b><?= $peserta['NamaLengkap'] ?></b></p>
+                                        <p> TTL : <b><?= $peserta['TempatLahir'] . ", " . $peserta['TglLahir'] ?></b></p>
+                                        <p> Jenis Kelamin : <b><?= $peserta['JenisKelamin'] ?></b></p>
+                                        <p> Sekolah Asal : <b><?= $peserta['AsalSekolah'] ?></b></p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                <?php
-                } else {
+                    <?php
+                        } else {
 
-                    ?>
-                    <h3 align="center">NOMOR REGISTRASI SALAH </h3>
-                <?php
-                }
-                ?>
+                            ?>
+                        <h3 align="center">NOMOR REGISTRASI SALAH </h3>
+                    <?php
+                        }
+                    } else {
+                        ?>
+                    <h3 align="center"> BELUM ADA PENGUMUMAN </h3>
+                <?php } ?>
 
 
             </div>
